a2290452f33f41bba5009d54c9a28e20040fab9e
katerji.abedalaziz@gmail.com <katerji.abedalaziz@gmail.com>
2025-03-11 22:45:58 +0400

    chore: add documentation

--- service/flight/service.go
+++ service/flight/service.go
@@ -14,7 +14,36 @@ func GetService() *Service {
 	return instance
 }
 
-func (s Service) FlightPath(flights []Flight) ([]string, bool) {
+// FindFlightPath	 reconstructs the travel itinerary from a given list of flight tickets.
+//
+// The function takes an array of Flight objects, where each Flight consists of a source and a destination airport code.
+// It returns the ordered sequence of airport codes representing the travel path and a boolean indicating if a valid itinerary was found.
+//
+// How It Works:
+// - It first validates the input flights using the validFlights function.
+// - It builds a mapping of destinations and a graph linking sources to destinations.
+// - It identifies the starting airport, which is the source that is not a destination in any ticket.
+// - It reconstructs the flight path by following the mapped connections in order.
+//
+// Parameters:
+// - flights ([]Flight): A list of Flight objects representing individual flight tickets.
+//
+// Returns:
+// - ([]string): The reconstructed travel itinerary in order of departure to final destination.
+// - (bool): A boolean indicating whether a valid itinerary was found.
+//
+// Example:
+// Given the input:
+//   flights = [
+//     Flight{source: "LAX", destination: "DXB"},
+//     Flight{source: "JFK", destination: "LAX"},
+//     Flight{source: "SFO", destination: "SJC"},
+//     Flight{source: "DXB", destination: "SFO"}
+//   ]
+//
+// The function returns:
+//   (["JFK", "LAX", "DXB", "SFO", "SJC"], true)
+func (s Service) FindFlightPath(flights []Flight) ([]string, bool) {
 	if !validFlights(flights) {
 		return nil, false
 	}
