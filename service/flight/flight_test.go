322c54617075da16dd9f5decd626b6cd4cd16c1b
katerji.abedalaziz@gmail.com <katerji.abedalaziz@gmail.com>
2025-03-11 22:32:26 +0400

    enhancement: modify valid test function to cover multiple different test cases

--- service/flight/flight_test.go
+++ service/flight/flight_test.go
@@ -5,12 +5,33 @@ import "testing"
 func TestFlightValid(t *testing.T) {
 	t.Parallel()
 
-	invalidFlight := Flight{
-		"DXB", "AUH", "BEY",
+	tests := []struct {
+		name string
+		f    Flight
+		want bool
+	}{
+		{
+			name: "valid flight",
+			f:    Flight{"DXB", "AUH"},
+			want: true,
+		},
+		{
+			name: "invalid flight with more than 2 destinations",
+			f:    Flight{"DXB", "AUH", "BEY"},
+			want: false,
+		},
+		{
+			name: "invalid flight with less than 2 destinations",
+			f:    Flight{"DXB"},
+			want: false,
+		},
 	}
-	valid := invalidFlight.valid()
-	if valid {
-		t.Error("expected flight to be invalid")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.f.valid(); got != tt.want {
+				t.Errorf("valid() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
