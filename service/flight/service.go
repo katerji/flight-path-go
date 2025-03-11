3bf5ae685fc1fc730e2b313fa98d760917a1c016
katerji.abedalaziz@gmail.com <katerji.abedalaziz@gmail.com>
2025-03-11 22:43:03 +0400

    feat: implement service method that returns flight path of an itinerary

--- service/flight/service.go
+++ service/flight/service.go
@@ -15,5 +15,44 @@ func GetService() *Service {
 }
 
 func (s Service) FlightPath(flights []Flight) ([]string, bool) {
-	return nil, false
+	if !validFlights(flights) {
+		return nil, false
+	}
+
+	destinations := map[string]bool{}
+	flightsGraph := map[string]string{}
+	for _, flight := range flights {
+		destinations[flight.destination()] = true
+		flightsGraph[flight.source()] = flight.destination()
+	}
+
+	var origin string
+	for _, flight := range flights {
+		if _, found := destinations[flight.source()]; !found {
+			origin = flight.source()
+			break
+		}
+	}
+
+	var flightsInOrder []string
+
+	currentFlight := origin
+	flightsInOrder = append(flightsInOrder, currentFlight)
+	for i := 0; i < len(flights); i++ {
+		nextDestination := flightsGraph[currentFlight]
+		flightsInOrder = append(flightsInOrder, nextDestination)
+		currentFlight = nextDestination
+	}
+
+	return flightsInOrder, true
+}
+
+func validFlights(flights []Flight) bool {
+	for _, f := range flights {
+		if !f.valid() {
+			return false
+		}
+	}
+
+	return true
 }
